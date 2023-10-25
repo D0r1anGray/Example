e08581b5d12b9a9db42b4e56559ab807ac3cef8a
Dorian Gray <d0r1an.gray@yandex.ru>
2023-10-25 21:17:32 +0300

    Программа для деления рандомных чисел double

---
+++ Sharp004Summ_with_User/Program.cs
@@ -0,0 +1,4 @@
+﻿//Console.WriteLine("Введите первое целое число: ");
+double NumberA = new Random().Next(1,10);
+double NumberB = new Random().Next(1,10);
+Console.WriteLine("Результат деления двух чисел: "+ (NumberA/NumberB));
