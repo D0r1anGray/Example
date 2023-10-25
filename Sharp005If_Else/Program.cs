624d648dae67363a90e3694dcc8f2cfbbaf665c5
Dorian Gray <d0r1an.gray@yandex.ru>
2023-10-25 21:28:39 +0300

    Рассмотрели условия if else вместе с .ToLower()

---
+++ Sharp005If_Else/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите своё имя!");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша"){
+    Console.WriteLine("Ура! Это же Маша!");
+}
+else{
+    Console.WriteLine("Ты чо здесь забыл, алабай-"+username+"? Ты не Маша!!!");
+}
