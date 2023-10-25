2995f492df6d8ae61ed97afbba441dd9c32ca0f2
Dorian Gray <d0r1an.gray@yandex.ru>
2023-10-25 20:55:33 +0300

    Добавили сумму чисел

---
+++ Sharp003Summ_numbers/Program.cs
@@ -0,0 +1,5 @@
+﻿//Console.WriteLine("Введите первое целое число:");
+int NumberA = 31;
+//Console.WriteLine("Введите второе целое число:");
+int NumberB = 56;
+Console.WriteLine("Сумма чисел = "+ (NumberA+NumberB));
